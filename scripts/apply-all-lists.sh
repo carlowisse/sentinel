03bf5b3d2d1ee281056bea7b132bf32714a5ecc7
Carlo Wisse <contact@carlowisse.com>
2023-10-07 13:13:03 +1100

    all

--- scripts/apply-all-lists.sh
+++ scripts/apply-all-lists.sh
@@ -5,21 +5,21 @@ BLACK_FILE=../lists/black/all_domains.txt
 WHITE_FILE=../lists/white/all_domains.txt
 
 echo "Loading blacklists..."
-while IFS= read -r LINE; do
-    ADDRESS="$LINE"
-    COMMENT=$(basename "$LINE")
+while IFS= read -r BLACK_LINE; do
+    BLACK_URL="$BLACK_LINE"
+    BLACK_COMMENT=$(basename "$BLACK_LINE")
 
-    sqlite3 /etc/pihole/gravity.db "INSERT INTO adlist (address, enabled, comment) VALUES ('$ADDRESS', 1, '$COMMENT');"
+    sqlite3 /etc/pihole/gravity.db "INSERT INTO adlist (address, enabled, comment) VALUES ('$BLACK_URL', 1, '$BLACK_COMMENT');"
 done <"$BLACK_FILE"
 
 echo "Loading whitelists..."
-while IFS= read -r LINE; do
-    URL="$LINE"
-    COMMENT=$(basename "$LINE")
+while IFS= read -r WHITE_LINE; do
+    WHITE_URL="$WHITE_LINE"
+    WHITE_COMMENT=$(basename "$WHITE_LINE")
 
-    curl "$URL" | while IFS=$'\r' read -r DOMAIN || [[ -n $DOMAIN ]]; do
-    pihole -w "$DOMAIN" --comment "$COMMENT" -nr
+    curl "$WHITE_URL" | while IFS=$'\r' read -r WHITE_DOMAIN || [[ -n $WHITE_DOMAIN ]]; do
+        pihole -w "$WHITE_DOMAIN" --comment "$WHITE_COMMENT" -nr
+    done
 done <"$WHITE_FILE"
-
 
 pihole -g
