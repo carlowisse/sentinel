2795df4b717fa2c0dbfaa8900a7f5f7e6eb019bc
Carlo Wisse <contact@carlowisse.com>
2023-10-07 12:51:10 +1100

    alllists

---
+++ scripts/apply-all-lists.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# sudo bash ./apply-all-list.sh
+
+FILE=../lists/black/all_domains.txt
+
+while IFS= read -r LINE; do
+    ADDRESS="$LINE"
+    COMMENT=$(basename "$LINE")
+
+    sqlite3 /etc/pihole/gravity.db "INSERT INTO adlist (address, enabled, comment) VALUES ('$ADDRESS', 1, '$COMMENT');"
+done <"$FILE"
